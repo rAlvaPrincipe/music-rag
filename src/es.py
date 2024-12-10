99abb00cc0abf7195fbc821ce6ccd921f98d3ea3
Renzo Alva Principe <renzo.alvaprincipe@datasinc.it>
2024-12-10 16:32:45 +0100

    index and insertion

--- src/es.py
+++ src/es.py
@@ -10,10 +10,11 @@ class ES:
         if not self.es.indices.exists(index=self.INDEX):
             mappings = {
                 "properties": {
-                    "page_id": {"type": "text", "analyzer": "standard"},
-                    "page": {"type": "text", "analyzer": "standard"},
+                    "source_id": {"type": "text", "analyzer": "standard"},
+                    "source_title": {"type": "text", "analyzer": "standard"},
+                    "source_url": {"type": "text", "analyzer": "standard"},
                     "text": {"type": "text", "analyzer": "english"},
-                    "embedding": {"type": "dense_vector", "dims": 768, "index": True, "similarity": "cosine"}
+                    "embedding": {"type": "dense_vector", "dims": 384, "index": True, "similarity": "cosine"}
                     }
                 }
             self.create_index(mappings, self.INDEX)
@@ -22,4 +23,13 @@ class ES:
     def create_index(self, mappings, index_name):
         self.es.indices.create(index=index_name, mappings=mappings)
         
+    
+    def insert(self, source_doc_id, source_doc_title, source_url, chunk_text,  embedding):  
+        doc = dict()
+        doc["source_id"] = source_doc_id
+        doc["source_title"] = source_doc_title
+        doc["source_url"] = source_url
+        doc["text"] = chunk_text
+        doc["embedding"] = embedding
+        resp = self.es.index(index=self.INDEX, document=doc)
     
