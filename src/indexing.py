016b3219b5366e79ddf9aacd0856b300f1c2a3d1
Renzo Alva Principe <renzo.alvaprincipe@datasinc.it>
2025-01-03 19:34:14 +0100

    idnexing pipeline now support multiple embedders

--- src/indexing.py
+++ src/indexing.py
@@ -5,7 +5,7 @@ from vectorizer import Vectorizer
 from es import ES
 from tqdm import tqdm
 from conf_indexing import parse, build_conf
-
+import hashlib
 
 def get_docs(source_path):
     docs = []
@@ -28,27 +28,41 @@ def doc2chunks(doc_text):
     return chunks
 
 
+def index_text(es, docs):
+    with tqdm(total=len(docs)) as pbar:
+        for doc in docs:
+            chunks = doc2chunks(doc["text"])
+            for chunk in chunks:
+                es.insert(doc["page_id"], doc["title"], doc["url"], chunk)
+            
+            pbar.update(1)
 
-def main():
-    args = parse()
-    conf = build_conf(args)
-    
-    source_path = "./data/english_alternative_rock_groups"
-    docs = get_docs(source_path)
-    
-    vectorizer = Vectorizer("sentence-transformers/all-MiniLM-L6-v2")
-    es = ES(conf)
-    
-    #TODO gestisci zero o molteplici vectorizers
-    
+
+def index_embeddings(es, docs, embedder_model):
+    vectorizer = Vectorizer(embedder_model)
     with tqdm(total=len(docs)) as pbar:
         for doc in docs:
             chunks = doc2chunks(doc["text"])
             embeddings = vectorizer.get_embeddings(chunks)
-            for chunk, embedding in zip(chunks, embeddings):
-                es.insert(doc["page_id"], doc["title"], doc["url"], chunk, embedding)
             
-            pbar.update(1)
+            for chunk, embedding in zip(chunks, embeddings):
+                id = doc["page_id"] + "_" + hashlib.md5(chunk.encode('utf-8')).hexdigest()
+                es.update_embedding(id, embedder_model, embedding)
+                pbar.update(1)
+
+
+def main():
+    args = parse()
+    conf = build_conf(args)
+    es = ES(conf)
+
+    docs = get_docs(conf["data_source"])[:30]
+    index_text(es, docs)
+    
+    embedders = conf["embedders"]
+    for embedder in embedders:
+        index_embeddings(es, docs, embedder)
+
 
 if __name__ == "__main__":
     main()
