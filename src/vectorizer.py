1bf8aafa9fbe8640c86ce42e738840a7fa2df4ac
Renzo Alva Principe <renzo.alvaprincipe@datasinc.it>
2025-01-03 19:31:58 +0100

    vectorizer refactory

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -6,11 +6,26 @@ from sentence_transformers import SentenceTransformer
 
 class Vectorizer():
 
-    available_models = ["sentence-transformers/all-MiniLM-L6-v2"] 
-
+    #available_models = ["sbert/all-MiniLM-L6-v2"] 
+    
+    AVAILABLE_MODELS = {
+        "sbert/all-MiniLM-L6-v2": {
+            "label": "sbert",
+            "id": "sentence-transformers/all-MiniLM-L6-v2",
+            "dims": 384
+        },
+        "sbert/all-mpnet-base-v2": {
+            "label": "sbert",
+            "id": "sentence-transformers/all-mpnet-base-v2",
+            "dims": 768
+        }
+    }
+    
+    
     def __init__(self, model):            
-        if model in self.available_models:
-            self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        if model in self.AVAILABLE_MODELS.keys():
+            if "sbert" in model:
+                self.model = SentenceTransformer(self.AVAILABLE_MODELS[model]["id"])
         else:
             raise Exception("model not available")
             
