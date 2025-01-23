40f78f609492ae88a8118ee625981b36343ffd1a
Renzo Alva Principe <r.alvaprincipe@campus.unimib.it>
2025-01-23 23:37:06 +0100

    added compatibility with openai

--- src/llms.py
+++ src/llms.py
@@ -1,4 +1,5 @@
 from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 import os 
 import json
 
@@ -7,9 +8,11 @@ with open(os.path.join("apikeys.json"), "r", encoding="utf-8") as f:
     keys = json.load(f) 
 
 os.environ["GROQ_API_KEY"] = keys["groq"]
-
+os.environ["OPENAI_API_KEY"] = keys["openai"]
 
 
 def get_llm(provider, model):
     if provider == "groq":
         return  ChatGroq( model=model, temperature=0)
+    elif provider == "openai":
+        return ChatOpenAI( model=model, temperature=0)
